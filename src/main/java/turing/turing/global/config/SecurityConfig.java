ed3856f4e64c110c3f1f7a69e7815116c6f77b22
beomzun <punpon53@naver.com>
2024-06-15 02:19:42 +0900

    Fix: CSRF 검증 비활성화

--- src/main/java/turing/turing/global/config/SecurityConfig.java
+++ src/main/java/turing/turing/global/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -14,7 +15,9 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((authorizeRequests) ->
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorizeRequests) ->
                 authorizeRequests.anyRequest().permitAll());
         return http.build();
 
