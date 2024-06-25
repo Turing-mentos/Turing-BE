76512d7c61a40cefd9ce9b90f750635159243b3a
mashin2002 <mashin0128@gmail.com>
2024-06-26 00:13:49 +0900

    [Fix]: 누락된 연관관계 추가

--- src/main/java/turing/turing/domain/schedule/Schedule.java
+++ src/main/java/turing/turing/domain/schedule/Schedule.java
@@ -2,9 +2,12 @@ package turing.turing.domain.schedule;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
@@ -13,6 +16,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import turing.turing.domain.BaseEntity;
+import turing.turing.domain.studyRoom.StudyRoom;
 
 @Getter
 @Entity
@@ -54,5 +58,10 @@ public class Schedule extends BaseEntity {
     @NotNull
     @Column(name = "session", nullable = false)
     private Integer session;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "study_room_id", nullable = false)
+    private StudyRoom studyRoom;
 
 }
