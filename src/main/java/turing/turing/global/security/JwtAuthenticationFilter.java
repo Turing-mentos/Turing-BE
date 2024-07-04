4bbc0051eca687bd134ee05765121bafa56adb15
beomzun <punpon53@naver.com>
2024-07-04 19:32:44 +0900

    [Feat]: jwt 토큰 검증

---
+++ src/main/java/turing/turing/global/security/JwtAuthenticationFilter.java
@@ -0,0 +1,49 @@
+package turing.turing.global.security;
+
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import turing.turing.domain.auth.jwt.JwtTokenProvider;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final static String HEADER_AUTHORIZATION = "Authorization";
+    private final static String TOKEN_PREFIX = "Bearer";
+
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
+
+        String token = getAccessToken(request);
+        if (token != null && jwtTokenProvider.validationToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+            throw new JwtException("토큰 유효성 검증 실패");
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getAccessToken(HttpServletRequest request) {
+        String token = request.getHeader(HEADER_AUTHORIZATION);
+        if (token == null) {
+            throw new NullPointerException();
+        } else if (token.startsWith(TOKEN_PREFIX)) {
+            return token.substring(7);
+        }
+
+        return null;
+    }
+}
