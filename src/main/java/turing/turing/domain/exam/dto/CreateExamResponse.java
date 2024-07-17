295812b27f1eb12ca405fcaada652db82d94fd83
beomzun <punpon53@naver.com>
2024-07-18 01:55:50 +0900

    [Fix]: 알림 공통 필드 사용 불가

--- src/main/java/turing/turing/domain/exam/dto/CreateExamResponse.java
+++ src/main/java/turing/turing/domain/exam/dto/CreateExamResponse.java
@@ -2,12 +2,15 @@ package turing.turing.domain.exam.dto;
 
 import lombok.Builder;
 import lombok.Getter;
-import turing.turing.domain.notice.dto.BaseNoticeInfo;
+import turing.turing.domain.Role;
 
 @Getter
 @Builder
 public class CreateExamResponse {
 
     private Long examId;
-    private BaseNoticeInfo baseNoticeInfo;
+    public Long senderId;
+    public Role senderRole;
+    public Long receiverId;
+    public Role receiverRole;
 }
