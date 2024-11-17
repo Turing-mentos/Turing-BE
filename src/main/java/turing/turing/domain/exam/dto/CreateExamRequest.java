9b3bdb0ff4c1fbd75ec03ee576295e1602486cf5
beomzun <punpon53@naver.com>
2024-11-17 15:01:12 +0900

    fix: add default constructor to serialization

--- src/main/java/turing/turing/domain/exam/dto/CreateExamRequest.java
+++ src/main/java/turing/turing/domain/exam/dto/CreateExamRequest.java
@@ -3,8 +3,10 @@ package turing.turing.domain.exam.dto;
 import java.time.LocalDate;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class CreateExamRequest {
 
